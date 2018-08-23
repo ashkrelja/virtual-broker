f183de970e630bf3779f9c93263d71f30cbb6c4b
unknown <ashkrelja@WSL.local>
2018-08-23 10:32:12 -0400

    changed backend DB to postgresql

--- virtual_broker/virtual_broker/settings.py
+++ virtual_broker/virtual_broker/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'virtual_broker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'db.postgresql'),
+        'USER':'mydatabaseuser',
+        'PASSWORD':'Economics3',
+        'HOST':'127.0.0.1',
+        'PORT':'5432'        
     }
 }
 
